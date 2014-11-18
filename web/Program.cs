cfe0faeb7cd73ea2c59d1637d373da3f8ba7b312
ton.yeung <tyeung@auto-master.com>
2014-11-18 10:20:24 -0600

    initial commit

---
+++ web/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace web
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<StartUpOwin>
+                (
+                    s =>
+                    {
+                        s.ConstructUsing(() => new StartUpOwin());
+                        s.WhenStarted((service, hostControl) => service.Start(hostControl));
+                        s.WhenStopped((service, hostControl) => service.Stop(hostControl));
+                    }
+                );
+
+                x.RunAsLocalSystem();
+                x.SetDescription("owin self host web project");
+                x.SetDisplayName("web-project");
+                x.SetServiceName("web-project");
+            });
+        }
+    }
+}
